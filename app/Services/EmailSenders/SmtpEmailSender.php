fc9a66b0d166f943611b7ee1f6770baefbc4606d
aghafforov <akmal.ghafforov@gmail.com>
2026-02-09 21:51:09 +0500

    feat: Implement actual email sending logic in SmtpEmailSender using Illuminate Mailer.

--- app/Services/EmailSenders/SmtpEmailSender.php
+++ app/Services/EmailSenders/SmtpEmailSender.php
@@ -2,20 +2,43 @@
 
 namespace App\Services\EmailSenders;
 
+use Illuminate\Support\Str;
+use Illuminate\Mail\Mailer;
+
 use App\DTOs\SendResult;
 use App\DTOs\BatchResult;
-use App\Contracts\EmailSenderInterface;
 use App\Contracts\Subscriber\Sendable;
+use App\Contracts\EmailSenderInterface;
 
 class SmtpEmailSender implements EmailSenderInterface
 {
+    public function __construct(
+        private Mailer $mailer
+    ) {}
+
     public function send(Sendable $subscriber, string $subject, string $htmlBody): SendResult
     {
-        return new SendResult('temp-id', 'sent');
+        $messageId = $this->generateMessageId();
+
+        $this->mailer->send(
+            ['html' => 'emails.raw'],
+            ['content' => $htmlBody],
+            function ($message) use ($subscriber, $subject) {
+                $message->to($subscriber->getEmail(), $subscriber->getName())
+                    ->subject($subject);
+            }
+        );
+
+        return new SendResult($messageId, 'sent');
     }
 
     public function sendBatch(array $recipients, string $subject, string $body): BatchResult
     {
         throw new \Exception('Not implemented');
+    }
+
+    private function generateMessageId(): string
+    {
+        return Str::uuid()->toString();
     }
 }
