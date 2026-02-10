7e344c11377e3947cb499df11ecb9ec35c05956e
aghafforov <akmal.ghafforov@gmail.com>
2026-02-10 11:15:49 +0500

    feat: Add scheduled commands for campaign subscriber collection, email sending, and status finalization.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('campaigns:collect-subscribers')->everyMinute();
+Schedule::command('campaigns:send-emails')->everyMinute();
+Schedule::command('campaigns:finalize-status')->everyFiveMinutes();
